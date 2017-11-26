b23e4ad72216aae2acb513f6031b7c9c5ee6de32
marricjudge <marric97@web.de>
2017-11-26 17:03:51 +0100

    finished
    finished exercise

--- ex36.py
+++ ex36.py
@@ -1,8 +1,10 @@
 from sys import exit
 
 def start():
+    print "--------------------------"
+    print " "
     print "You are walking in the forest and a little monster jumpes out of the woods."
-    print "Monster: 'I won't let you pass here.' ", "Unless you're paying me 100 Euros."
+    print "Monster: 'I won't let you pass here. Unless you're paying me 100 Euros.' "
     print "--------------------------- "
     print "What do you do? Do you either PAY, RUN or FIGHT?"
     print "Type one of the options here."
@@ -14,11 +16,11 @@ def start():
             pay()
             break
         elif pay_run_fight_input == "RUN":
-            #function Here
-            print "run successfull"
+            run()
+            break
         elif pay_run_fight_input == "FIGHT":
-            #function here
-            print "fight sucessfull"
+            fight()
+            break
         elif i <= 4:
             print "Please try again"
         else:
@@ -26,40 +28,73 @@ def start():
 
 def pay():
     print "Pay? Are you rich? Yes or No?"
-    are_you_rich_input = raw_input("> ").upper()
-    print "After entering the input"
-    print are_you_rich_input
 
-    if are_you_rich_input == "YES" or "1" or "Y":
-        print "Yes? Well then you can afford it."
-        success()
-    elif are_you_rich_input == "NO" or "0" or "N":
+    are_you_rich_input = raw_input("> ").upper()
+    #print "After entering the input"
+    #print are_you_rich_input
+    if are_you_rich_input == "YES":
+        success("Yes? Well, then you can afford it.")
+    elif are_you_rich_input == "NO":
         dead("No? Too bad. It's human for dinner")
+    else:
+        dead("The monster loves to eat humans with typing mistakes!")
+
 
 def run():
     print "Are you trained enough to run away? Yes or No?"
 
     trained_input = raw_input("> ").upper()
 
-    if trained_input == "YES" or "1" or "Y":
-        print "Better get your running shoes on then."
-        success()
-    elif trained_input == "NO" or "0" or "N":
-        dead("Too bad. It's human for dinner")
+    if trained_input == "YES":
+        success("Better get your running shoes on then.")
+    elif trained_input == "NO":
+        dead("Too bad. The monster catched you on your run")
+    else:
+        dead("Too slow. The monster loves to eat humans with typing mistakes!")
+
+
+def fight():
+    print "Fight? Ok. Are you strong?"
+    strong_input = raw_input("> ").upper()
+
+    if strong_input == "YES":
+        dead ("While warming up your muscles the monster hit you to death.")
+    elif strong_input == "NO":
+        smart()
+    else:
+        dead("While inserting the wrong input the monster hit you to death.")
+
+def smart():
+    print "Well, if you're not strong then at least smart?"
+    smart_input = raw_input("> ").upper()
+
+    if smart_input == "YES":
+        success("You tricked the monster.")
+    elif smart_input == "NO":
+        dead ("Either strong nor smart - and no luck today")
 
 def dead(why):
     print why
+    print " "
+    print "-------------------"
+    print " "
     print "Thanks for playing. Wanna try again? Yes or No?"
-    play_again_input = raw_input("> ")
-    if play_again_input == "YES" or "1" or "Y":
+    play_again_input = raw_input("> ").upper()
+    if play_again_input == "YES":
         print "Ok, let's go"
         print " "
         start()
-    elif play_again_input == "NO" or "0" or "N":
+    else:
+        print "Good bye"
         exit(0)
 
-def success():
+def success(why):
+    print why
+    print " "
+    print "----------------------------"
+    print " "
     print "Yeah. You made it. Congraz."
     print "Thanks for playing."
+    exit(0)
 
 start()
